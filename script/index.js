c2a9794ec2b38f2c87b32c8e97829834f45611ef
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-11-20 21:02:21 +0200

    adding pages

---
+++ script/index.js
@@ -0,0 +1,28 @@
+'use strict';
+let side=true
+document.querySelector('.toggle')
+        .addEventListener('click', function() {
+  this.classList.toggle('activate');
+  if(side){
+    document.getElementById("myDIV").style.display = "block";
+    side=false
+  }
+  else{
+    document.getElementById("myDIV").style.display = "none";
+    side=true
+  }
+ 
+});
+document.querySelector('.sidebaroption')
+        .addEventListener('click', function() {
+  this.classList.toggle('activate');
+  if(side){
+    document.getElementById("myDIV").style.display = "block";
+    side=false
+  }
+  else{
+    document.getElementById("myDIV").style.display = "none";
+    side=true
+  }
+ 
+});
