293e06533b85fb17698d7521b7051cf88afbcd41
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2022-11-20 21:02:21 +0200

    adding pages

---
+++ script/article.js
@@ -0,0 +1,7 @@
+tinymce.init({
+  selector: "textarea.simple",
+  plugins: "advlist lists spellchecker autoresize wordcount",
+  min_height: 120,
+  menubar: false,
+  toolbar: "bold italic underline | bullist numlist | spellchecker"
+});
